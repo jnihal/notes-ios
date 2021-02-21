51dba320eeea5cf3dcd62fd1dbf18a47789a512d
Nihal Jain <nihaljain222@gmail.com>
2021-02-21 13:18:51 +0530

    First commit

---
+++ Notes/NotesListViewController.swift
@@ -0,0 +1,52 @@
+import UIKit
+
+class NotesListViewController: UITableViewController {
+    var notes: [Note] = []
+    
+    @IBAction func createNote() {
+        let _ = NoteManager.shared.create()
+        reload()
+    }
+    
+    func reload() {
+        notes = NoteManager.shared.getNotes()
+        tableView.reloadData()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        reload()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NoteCell", for: indexPath)
+        cell.textLabel?.text = notes[indexPath.row].content
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let delete = UITableViewRowAction(style: .default, title: "Delete") { [self] (action, indexPath) in
+            NoteManager.shared.deleteNote(note: notes[indexPath.row])
+            reload()
+        }
+        delete.backgroundColor = UIColor.red
+        
+        return [delete]
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "NoteSegue",
+                let destination = segue.destination as? NoteViewController,
+                let index = tableView.indexPathForSelectedRow?.row {
+            destination.note = notes[index]
+        }
+    }
+}
